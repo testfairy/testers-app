e4f1ab2a78348633f9e13b5365f44eee1710f37e
Vijay Sharma <vijay.sharm@gmail.com>
2021-10-17 11:41:20 -0400

    Create new testers app wrapper

---
+++ TestFairy/TestFairyApp.swift
@@ -0,0 +1,16 @@
+//
+//  TestFairyApp.swift
+//  TestFairy
+//
+//
+
+import SwiftUI
+
+@main
+struct TestFairyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
